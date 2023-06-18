854e970ec9ba855502dcf8c1d93bbba34675e9e3
ilyas <ilyas.saadi9309@gmail.com>
2023-06-18 16:02:42 -0700

    question é

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*2. I sometimes suffer from insomnia. And when I can't fall asleep
+ * , I play what I call the alphabet game*/
+int main() {
+    char caracter;
+    int i;
+    for(i=97;i<123;i++)
+    {caracter=i;
+        putchar(caracter);
+    }
+    return 0;
+}
