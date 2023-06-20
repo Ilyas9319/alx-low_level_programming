a112ba838a25126e64be47f61f97d4bc119d5728
ilyas <ilyas.saadi9309@gmail.com>
2023-06-20 10:52:24 -0700

    print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
