d67a5ba22f91eea378e66709a034bd7d5c774754
ilyas <ilyas.saadi9309@gmail.com>
2023-06-20 13:42:28 -0700

    putshar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,11 @@
  */
 void print_alphabet(void)
 	{
-		int i;
+	char letter;
 
-		for (i = 97; i < 123; i++)
-		{
-			putchar(i);
-		}
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
 	}
