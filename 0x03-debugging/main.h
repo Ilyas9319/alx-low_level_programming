717b77c0ce4a3423e1ac5ec2f9e500134125fecb
ilyas <ilyas.saadi9309@gmail.com>
2023-06-22 11:47:26 -0700

    largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,28 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
 
 #endif /* MAIN_H */
