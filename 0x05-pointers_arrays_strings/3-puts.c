f3dec51938cd6a67dff187635d003cfde24c4dd5
ilyas <ilyas.saadi9309@gmail.com>
2023-06-26 09:03:26 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _puts - Prints a string to the standard output.
+* @str: Pointer to the string.
+*/
+void _puts(char *str)
+{
+	printf("%s", str);
+}
